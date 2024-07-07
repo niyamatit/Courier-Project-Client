67f00313e6919692198c43cb2244000d0f04f226
codermahbub20 <mkrefat5@gmail.com>
2024-07-07 20:30:58 +0600

    Merchant dashboard layout add

---
+++ src/Components/DashBoard/SideBar/MerchantMenu.jsx
@@ -0,0 +1,57 @@
+import { FaCartPlus, FaClipboardList, FaSearchPlus, FaShareAltSquare } from "react-icons/fa";
+import MenuItem from "./MenuItem";
+import { MdAccountBalance, MdAddBox } from "react-icons/md";
+import { TbCoinTaka } from "react-icons/tb";
+
+
+const MerchantMenu = () => {
+    return (
+        <>
+      <MenuItem
+        icon={ MdAddBox }
+        label='Add Parcel'
+        address='/'
+      />
+
+      {/* <MenuItem
+        icon={MdHomeWork}
+        label='My Listings'
+        address='my-listings'
+      /> */}
+
+      <MenuItem
+        icon={FaCartPlus}
+        label='Bulk Entry'
+        address='/'
+      />
+
+      <MenuItem
+        icon={FaClipboardList}
+        label='All Orders'
+        address='/'
+      />
+      <MenuItem
+        icon={MdAccountBalance}
+        label='Accounts'
+        address='/'
+      />
+      <MenuItem
+        icon={FaSearchPlus}
+        label='Order Tracking'
+        address='/'
+      />
+      <MenuItem
+        icon={FaShareAltSquare}
+        label='Coverage Area'
+        address='/'
+      />
+      <MenuItem
+        icon={TbCoinTaka}
+        label='Service Charge'
+        address='/'
+      />
+    </>
+    );
+};
+
+export default MerchantMenu;
