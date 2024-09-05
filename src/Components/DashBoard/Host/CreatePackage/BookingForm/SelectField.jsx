84d7a2e0af3eb1e201bedcdf0475bbf5443b439a
afsana-jannat <afsanajannat205@gmail.com>
2024-09-06 01:21:35 +0600

    booking form submit

--- src/Components/DashBoard/Host/CreatePackage/BookingForm/SelectField.jsx
+++ src/Components/DashBoard/Host/CreatePackage/BookingForm/SelectField.jsx
@@ -1,14 +1,19 @@
-const SelectField = ({ label, options, required = false }) => (
+const SelectField = ({ label, options, required = false, register, name, registerOptions, errors, watchValues }) => (
     <div className="form-control">
         <label className="label">
             <span className="label-text text-gray-500 font-semibold">{label}</span>
         </label>
-        <select className="select select-bordered bg-[#E8F0FE] text-black" required={required}>
+        <select className="select select-bordered bg-[#E8F0FE] text-black"
+            // required={required}
+            {...register(name, registerOptions)}
+        >
             <option disabled selected>{label}</option>
             {options.map((option, index) => (
                 <option key={index}>{option}</option>
             ))}
+
         </select>
+        {errors[name] && !watchValues[name] && <p className="text-red-500">This field is required</p>}
     </div>
 );
 
