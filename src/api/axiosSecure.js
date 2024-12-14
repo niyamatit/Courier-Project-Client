588d5533d3378ab53ef3c3c82534e81095bf6b45
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2024-12-14 12:53:21 +0600

    bug fixed

--- src/api/axiosSecure.js
+++ src/api/axiosSecure.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const axiosSecure = axios.create({
-  //  baseURL: 'https://courier-server-rho.vercel.app',
-// VITE_API = https://courier-server-rho.vercel.app
+ 
    baseURL: import.meta.env.VITE_API,
   // baseURL: 'http://localhost:5000',
   withCredentials: true,
