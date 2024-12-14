cf91e9e97126ac0b628f8a5909109775082af773
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2024-12-14 20:37:43 +0600

    bug fixed

--- src/api/axiosSecure.js
+++ src/api/axiosSecure.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
+
 const axiosSecure = axios.create({
-  baseURL: import.meta.env.VITE_API,
+ 
+   baseURL: import.meta.env.VITE_API,
+  // baseURL: 'http://localhost:5000',
   withCredentials: true,
-  validateStatus: (status) => {
-    // Treat all responses as valid unless status is 500 or above
-    return status < 500;
-  },
 });
 
 export default axiosSecure;
-
 
