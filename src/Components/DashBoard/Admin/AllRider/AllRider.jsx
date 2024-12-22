a612dd809887913f46a960737e6b99080401d073
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2024-12-22 21:03:14 +0600

    All rider details added [Admin Dashboard]

--- src/Components/DashBoard/Admin/AllRider/AllRider.jsx
+++ src/Components/DashBoard/Admin/AllRider/AllRider.jsx
@@ -33,6 +33,9 @@ const AllRider = () => {
                                             SL
                                         </th>
                                         <th className='px-5 py-3 bg-white border-b border-gray-200 text-gray-800 text-left text-sm uppercase font-normal'>
+                                            Date
+                                        </th>
+                                        <th className='px-5 py-3 bg-white border-b border-gray-200 text-gray-800 text-left text-sm uppercase font-normal'>
                                             Rider Image
                                         </th>
                                         <th className='px-5 py-3 bg-white border-b border-gray-200 text-gray-800 text-left text-sm uppercase font-normal'>
