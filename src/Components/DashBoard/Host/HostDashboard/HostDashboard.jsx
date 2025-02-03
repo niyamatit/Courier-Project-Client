5f2705943ee16a752b537e430ea90042161d28d6
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-02-03 22:15:02 +0600

    today pickup data showed  [Branch Dashboard]

--- src/Components/DashBoard/Host/HostDashboard/HostDashboard.jsx
+++ src/Components/DashBoard/Host/HostDashboard/HostDashboard.jsx
@@ -265,7 +265,7 @@ const HostDashboard = () => {
             </div>
 
 
-            <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 p-5">
+              <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 p-5">
                 <HostStatsCard
                     title="Today Pickup Request"
                     icon={<FaTruckPickup />}
