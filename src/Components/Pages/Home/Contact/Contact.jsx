97eebb8e2a297fad1f49e97a762227ae3f3f8ef1
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-02-13 20:57:31 +0600

    bug fixed

--- src/Components/Pages/Home/Contact/Contact.jsx
+++ src/Components/Pages/Home/Contact/Contact.jsx
@@ -59,7 +59,7 @@ const Contact = () => {
               {errMsg}
             </p>
           )}
-          {successMsg && (
+           {successMsg && (
             <p className="py-3 bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne text-center text-green-500 text-base tracking-wide animate-bounce">
               {successMsg}
             </p>
