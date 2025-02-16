ff625e628d341d41f642a6a3dab00929e8fb1a7e
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-02-16 23:56:23 +0600

    cn updated

--- src/Components/DashBoard/Host/CreatePackage/StickerDetails.jsx
+++ src/Components/DashBoard/Host/CreatePackage/StickerDetails.jsx
@@ -66,7 +66,7 @@ const StickerDetails = ({ bookingInfo }) => {
             <div className="mt-2 border-t pt-2">
                 <p className="text-xs text-gray-700"><strong>Product:</strong> {bookingInfo?.productDetails}</p>
                 <p className="text-xs text-gray-700"><strong>Condition Charge:</strong> {bookingInfo?.conditionCharge} BDT</p>
-                <p className="text-xs text-gray-700"><strong>Total Amount:</strong> {bookingInfo?.amount} BDT ({bookingInfo?.wordAmount})</p>
+                {/* <p className="text-xs text-gray-700"><strong>Total Amount:</strong> {bookingInfo?.amount} BDT ({bookingInfo?.wordAmount})</p> */}
             </div>
 
             {/* QR Code Section */}
