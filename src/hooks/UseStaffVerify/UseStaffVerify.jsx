2ae32330a02337325552fdf3f61731ce2242c228
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-02-18 19:24:29 +0600

    VerifiedStaff Functionality added

--- src/hooks/UseStaffVerify/UseStaffVerify.jsx
+++ src/hooks/UseStaffVerify/UseStaffVerify.jsx
@@ -1,10 +1,39 @@
 
+import { useQuery } from "@tanstack/react-query";
+import axiosSecure from "../../api/axiosSecure";
+import useUsersData from "../useUsersData/useUsersData";
+
+
 const UseStaffVerify = () => {
-    return (
-        <div>
-           <h1>Hellll</h1> 
-        </div>
-    );
+    const deobfuscatePassword = (obfuscatedPassword) => {
+        let actualPassword = "";
+        for (let i = 0; i < obfuscatedPassword.length; i += 21) {
+          actualPassword += obfuscatedPassword[i]; 
+        }
+        return actualPassword;
+      };
+    const StaffUserID = (localStorage.getItem('StaffEmail'))
+    const StaffPassword = (localStorage.getItem('StaffPassword'))
+    // console.log("LOcal Email:",UserEmail)
+    const [verifiedUser] = useUsersData();
+     const {  data: staffs = [], isLoading} = useQuery({
+        queryKey: ['staffs'],
+        queryFn: async() => {
+            const res = await axiosSecure.get("/staffkdgfdjhksgfjhkdjkjkfhfjk");
+            return res.data;
+        }
+
+    });
+    const verifiedStaff = staffs.find((staff) =>
+        staff?.Staff_Branch_Name === verifiedUser?.email &&
+        staff?.Staff_User_ID === StaffUserID &&
+        deobfuscatePassword(staff?.Staff_Password) === StaffPassword)
+    console.log("All Verified user ",verifiedStaff)
+    
+
+
+    return [verifiedStaff, isLoading]
+
 };
 
 export default UseStaffVerify;
