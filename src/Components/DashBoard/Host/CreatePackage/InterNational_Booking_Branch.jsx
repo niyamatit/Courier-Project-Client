d040a0618158953129cb1630cc4355b6136843c6
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-02-27 21:37:41 +0600

    Inter National Booking route added [Branch Dashboard]

---
+++ src/Components/DashBoard/Host/CreatePackage/InterNational_Booking_Branch.jsx
@@ -0,0 +1,11 @@
+
+
+const InterNational_Booking_Branch = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    );
+};
+
+export default InterNational_Booking_Branch;
