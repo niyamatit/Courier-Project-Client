37b29e29751bcee5c3c6be22887a1f36ba264126
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-03-12 23:15:10 +0600

    Merchant list added in branch [Branch Dashboard]

--- src/Components/DashBoard/Host/MerchantList/MerchantList.jsx
+++ src/Components/DashBoard/Host/MerchantList/MerchantList.jsx
@@ -1,29 +1,50 @@
 import { useQuery } from "@tanstack/react-query";
 import { getAllUsers } from "../../../../api/auth";
 import MerchantDataRow from "./MerchantDataRow";
+import axiosSecure from "../../../../api/axiosSecure";
+import useUsersData from "../../../../hooks/useUsersData/useUsersData";
 
 
 const MerchantList = () => {
-
-    const { data: users = [], refetch } = useQuery({
-        queryKey: ['users'],
-        queryFn: async () => await getAllUsers(),
-      });
+  const [verifiedUser] = useUsersData();
+  const { data: users = [],refetch } = useQuery({
+    queryKey: ['users'],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/shfjksdhfjdjkfhxnbcnbc67437gch`);
+      return res.data;
+    }
+  })
+  
       
       // Filter the users to only include those with the role of 'rider'
-      const riders = users.filter(user => user.role === 'merchant');
+      const merchants = users.filter(user => 
+        
+        user.Merchant_Branch === verifiedUser?.name
+      );
 
-
+  
     return (
         <>
         <div className='container mx-auto px-4 sm:px-8'>
-         
+         <h1 className="text-3xl text-center mt-10 font-bold">My Merchants</h1>
           <div className='py-8'>
             <div className='-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto'>
               <div className='inline-block min-w-full shadow rounded-lg overflow-hidden'>
                 <table className='min-w-full leading-normal'>
                   <thead>
                     <tr>
+                      <th
+                        scope='col'
+                        className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                      >
+                        SL
+                      </th>
+                      <th
+                        scope='col'
+                        className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                      >
+                        imageUrl
+                      </th>
                       <th
                         scope='col'
                         className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
@@ -40,25 +61,21 @@ const MerchantList = () => {
                         scope='col'
                         className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
                       >
-                        Role
+                        Address
                       </th>
-                      <th
-                        scope='col'
-                        className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
-                      >
-                        Status
-                      </th>
+                      
   
                      
                     </tr>
                   </thead>
                   <tbody>
                       {/* User data table row */}
-                      {riders &&
-                      riders.map(user => (
+                      {merchants &&
+                      merchants.map((user , index) => (
                         <MerchantDataRow
                           key={user._id}
                           user={user}
+                          index={index}
                           refetch={refetch}
                         />
                       ))}
