cca3e0ccf8a3790ecd462524aacc57803a90accf
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-03-23 16:19:04 +0600

    Online booking list Cn Number added

--- src/Components/DashBoard/Host/CreatePackage/BranchBookinginfo/BookingInfo/BookingInfo.jsx
+++ src/Components/DashBoard/Host/CreatePackage/BranchBookinginfo/BookingInfo/BookingInfo.jsx
@@ -84,6 +84,9 @@ const BookingInfo = () => {
                                         Product Details
                                     </th>
                                     <th className='px-5 py-3 bg-white border-b border-gray-200 text-gray-800 text-left text-sm uppercase font-normal'>
+                                        CN Number
+                                    </th>
+                                    <th className='px-5 py-3 bg-white border-b border-gray-200 text-gray-800 text-left text-sm uppercase font-normal'>
                                         Actions
                                     </th>
                                     <th className='px-5 py-3 bg-white border-b border-gray-200 text-gray-800 text-left text-sm uppercase font-normal'>
