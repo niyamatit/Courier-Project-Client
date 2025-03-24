f03c7fadfe66a0f40e8e7a7ad146169d322b7e1e
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-03-24 23:20:33 +0600

    recharge-complete route beautiful table added with functionality [Admin Dashbord]

--- src/Components/DashBoard/Host/Recharge/RechargeComplete/Recharge_History.jsx
+++ src/Components/DashBoard/Host/Recharge/RechargeComplete/Recharge_History.jsx
@@ -21,7 +21,7 @@ const Recharge_History = () => {
                             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Branch</th>
                             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Account</th>
                             <th className="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">Total Branch Amount (৳)</th>
-                            <th className="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">Now Added (৳)</th>
+                             <th className="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">Now Added (৳)</th>
                             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Note</th>
                             
                             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Date</th>
