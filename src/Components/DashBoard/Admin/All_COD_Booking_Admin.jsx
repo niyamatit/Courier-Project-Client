d78ce9e30cee7c06cdd946071e2932f3b0c973b0
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-03-26 12:41:45 +0600

    Loaded Online and Offline Both Data On All COD Booking route [Admin Dashboard]

--- src/Components/DashBoard/Admin/All_COD_Booking_Admin.jsx
+++ src/Components/DashBoard/Admin/All_COD_Booking_Admin.jsx
@@ -1,6 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+import { getOffline, getPackage } from "../../../api/auth";
 
 
 const All_COD_Booking_Admin = () => {
+    const {
+        data: OnlineBookings = [],
+        isLoading,
+    } = useQuery({
+        queryKey: ['OnlineBookings',],
+       
+        queryFn: async () => await getPackage(),
+       
+    });
+     const {
+            data: OfflineBookings = [],
+            isLoading,
+        } = useQuery({
+            queryKey: ["OfflineBookings"],
+          
+            queryFn: async () => await getOffline(),
+           
+        });
     return (
         <div>
          <h1>Hello</h1>   
