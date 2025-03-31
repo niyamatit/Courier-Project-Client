2f9eb20b52bd0e3bd81eabbafd53681c16344e6f
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-03-31 21:46:30 +0600

    Footer Updated

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   
 
   return (
-    <footer className="bg-blue-400 text-white">
+     <footer className="bg-blue-400 text-white">
       <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
