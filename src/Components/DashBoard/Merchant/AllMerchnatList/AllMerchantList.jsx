8c73a1307169411c454cbdaf04bc588ba8518558
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-05-20 19:40:55 +0600

    Merchant Recharge Apply router added

--- src/Components/DashBoard/Merchant/AllMerchnatList/AllMerchantList.jsx
+++ src/Components/DashBoard/Merchant/AllMerchnatList/AllMerchantList.jsx
@@ -236,7 +236,7 @@ const AllMerchantList = () => {
                                         className="border border-gray-300 px-3 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150 ease-in-out"
                                     />
                                 </td>
-                                 <td className="px-6 py-4">{merchant.Merchant_Balance}</td>
+                                 <td className="px-6 py-4">{merchant?.Merchant_Balance}</td>
                                 <td className="px-6 py-4 text-center">
                                     <button
                                         onClick={() => handleRemove(merchant._id)}
