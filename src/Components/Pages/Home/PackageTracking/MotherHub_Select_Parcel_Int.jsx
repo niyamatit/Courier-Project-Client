4bdd44145f48fac6268aa89f13b0e9d4bfd7f676
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-07-09 23:22:30 +0600

    MotherHub_Select_Parcel_Int route created

---
+++ src/Components/Pages/Home/PackageTracking/MotherHub_Select_Parcel_Int.jsx
@@ -0,0 +1,11 @@
+
+
+const MotherHub_Select_Parcel_Int = () => {
+    return (
+        <div>
+          <h1>jdfhjfjjhfjhg</h1>  
+        </div>
+    );
+};
+
+export default MotherHub_Select_Parcel_Int;
