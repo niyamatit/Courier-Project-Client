96579f29919124a4533aad9e9a7069505719c592
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-07-10 19:48:38 +0600

    Pending_Parecl_List_International route created

---
+++ src/Components/Pages/Home/PackageTracking/Pending_Parecl_List_International.jsx
@@ -0,0 +1,11 @@
+
+
+const Pending_Parecl_List_International = () => {
+    return (
+        <div>
+            <h1>kgfkd</h1>
+        </div>
+    );
+};
+
+export default Pending_Parecl_List_International;
