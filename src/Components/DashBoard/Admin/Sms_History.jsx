40cd7839768a57ed5a3e33fd45421b79cb6304eb
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-07-28 22:26:02 +0600

    SMS History Route Deisgn Added [Admin Dashboard]

--- src/Components/DashBoard/Admin/Sms_History.jsx
+++ src/Components/DashBoard/Admin/Sms_History.jsx
@@ -1,6 +1,15 @@
+import { useQuery } from "@tanstack/react-query";
+import axiosSecure from "../../../api/axiosSecure";
 
 
 const Sms_History = () => {
+    const { data: SMS_History = [],refetch: refetchUsers } = useQuery({
+    queryKey: ['SMS_History'],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/sms");
+      return res.data;
+    }
+  })
     return (
         <div>
             <h1>SMS History</h1>
