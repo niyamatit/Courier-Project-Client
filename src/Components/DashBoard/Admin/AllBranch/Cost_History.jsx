b057115bbed84b27731ea6df195a6933e14e47c7
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-08-03 22:35:18 +0600

    Cost_History deisgn added [admin]

--- src/Components/DashBoard/Admin/AllBranch/Cost_History.jsx
+++ src/Components/DashBoard/Admin/AllBranch/Cost_History.jsx
@@ -1,6 +1,15 @@
+import { useQuery } from "@tanstack/react-query";
+import axiosSecure from "../../../../api/axiosSecure";
 
 
 const Cost_History = () => {
+    const { data: Cost_History = [] } = useQuery({
+    queryKey: ['Cost_History'],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/cost");
+      return res.data;
+    }
+  });
     return (
         <div>
            <h1>AAA</h1> 
