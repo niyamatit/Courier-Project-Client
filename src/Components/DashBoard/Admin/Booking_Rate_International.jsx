f822830ae7cc3272ec3c4f4a55df2fd40c8a48ad
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-08-09 18:16:19 +0600

    International Booking Rate Route Created

---
+++ src/Components/DashBoard/Admin/Booking_Rate_International.jsx
@@ -0,0 +1,11 @@
+
+
+const Booking_Rate_International = () => {
+    return (
+        <div>
+            <h1>Hjhdjshjkdfbh</h1>
+        </div>
+    );
+};
+
+export default Booking_Rate_International;
