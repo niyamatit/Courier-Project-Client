a3f2bfd28d88f6e36960a2afafd5764f27071003
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-08-12 19:22:32 +0600

    international booking branch error fixed

--- src/Components/DashBoard/Host/CreatePackage/InterNational_Booking_Branch.jsx
+++ src/Components/DashBoard/Host/CreatePackage/InterNational_Booking_Branch.jsx
@@ -330,7 +330,7 @@ const senderUrl = `${SMS_API}?api_key=${API_KEY}&type=text&number=${Number(data?
     
     SMS_Staus: {
       Sender: senderRes.data,
-        Receiver: receiverRes.data  
+        // Receiver: receiverRes.data || '',  
     },
     senderMobile: Number(data?.SenderContactINT),
      Purpuse: "International Booking",
