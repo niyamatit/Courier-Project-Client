d1e14e68eb68a7db7b1aa8856fe018b395cfa705
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-08-12 23:36:45 +0600

    duplicate cn Number Error fixed

--- src/Components/DashBoard/Host/CreatePackage/InterNational_Booking_Branch.jsx
+++ src/Components/DashBoard/Host/CreatePackage/InterNational_Booking_Branch.jsx
@@ -305,8 +305,8 @@ const InterNational_Booking_Branch = () => {
 
         // ------------------------------------SMS------------------------------------------
           // Step 5: Send SMS using BulkSMSBD
-const SMS_API = "demo";
-// const SMS_API = "https://bulksmsbd.net/api/smsapi";
+// const SMS_API = "demo";
+const SMS_API = "https://bulksmsbd.net/api/smsapi";
 const API_KEY = "VSkytluAnQbG0vsCEbHQ";
 const SENDER_ID = "8809617624950";
 
