13a6c07fbf0d4e5a41f4474b97034870dccf0cb1
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2026-01-12 23:26:09 +0600

    Add Account Merchant Route Added

---
+++ src/Components/DashBoard/Merchant/AddAccount/Add_Account_Merchant.jsx
@@ -0,0 +1,11 @@
+
+
+const Add_Account_Merchant = () => {
+    return (
+        <div>
+            <h1>Add Account Merchant Dashboard </h1>
+        </div>
+    );
+};
+
+export default Add_Account_Merchant;
