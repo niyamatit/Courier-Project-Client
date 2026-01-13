5613264f69ef4425482dca02496a06abe61c3bce
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2026-01-13 18:53:23 +0600

    Merchant Account List Route initially added in Admin

---
+++ src/Components/DashBoard/Merchant/AllMerchantAccountList/AllMerchantAccountList.jsx
@@ -0,0 +1,11 @@
+
+
+const AllMerchantAccountList = () => {
+    return (
+        <div>
+           <h1>AllMerchantAccountList</h1> 
+        </div>
+    );
+};
+
+export default AllMerchantAccountList;
