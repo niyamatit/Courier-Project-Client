433b3e4650188e81952e1eb73d5a07dc6a110fa5
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2026-02-12 22:46:34 +0600

    bug fixed

--- src/Components/DashBoard/Host/CreatePackage/OnlineBooking_Merchant.jsx
+++ src/Components/DashBoard/Host/CreatePackage/OnlineBooking_Merchant.jsx
@@ -62,6 +62,7 @@ const OnlineBooking_Merchant = () => {
     const [selectedArea, setSelectedArea] = useState("");
     const [isEditingCod, setIsEditingCod] = useState(false);
 const [collectionAmount, setCollectionAmount] = useState("");
+const [collectionAmountagain, setCollectionAmountagain] = useState("");
 
     const [DeliveryComplete, setDeliveryComplete] = useState(0);
         const [DeliveryPending, setDeliveryPending] = useState(0);
