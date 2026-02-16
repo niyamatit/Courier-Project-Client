8dd6114f111d63ef9c3a005d3daad49445d57585
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2026-02-17 00:06:13 +0600

    Base Api Updated

--- src/api/axiosSecure.js
+++ src/api/axiosSecure.js
@@ -3,7 +3,7 @@ import axios from 'axios';
  const axiosSecure = axios.create({
  
   
-  //  baseURL: import.meta.env.VITE_API,
+   baseURL: import.meta.env.VITE_API,
    // baseURL: import.meta.env.VITE_API,
     // baseURL: 'http://localhost:5000',
   withCredentials: true,
