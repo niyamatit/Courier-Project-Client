95b91679cc55021c42939846c4588cc221ab6171
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2026-06-26 22:47:27 +0600

    Merchant Balance First Step Done

--- src/Components/DashBoard/Merchant/MerchantAddPercel/MerchantAddPercel.jsx
+++ src/Components/DashBoard/Merchant/MerchantAddPercel/MerchantAddPercel.jsx
@@ -861,7 +861,7 @@ const incrementCnNumber = (cnNumber) => {
       Delivary_Charge: deliveryCharge || 0,
       Total_Charge: finalCharge || 0,
       isProcessed:false,
-       Calculate_Charge_Merchant: parseFloat(finalCharge)  - parseFloat(formData?.totalAmount),
+       Calculate_Charge_Merchant:parseFloat(formData?.totalAmount)- parseFloat(finalCharge),
       deliveryCharge: deliveryPrice || 0,
       Delivery_Option: deliveryOption || "",
       Quantity: quantity || 1,
